d5697e2359240090c7d2833348303bbd68acc98e
LUXINTERACTIVE\KGovindarasu <Karthikeyan.Govindarasu@LuxInteractive.com>
2019-03-09 21:35:03 -0500

    Ngrx Store added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './store/reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './store/app.effects';
+import { AppEffects } from './store/effects/app.effects';
+import { HomeEffects } from './store/effects/home.effects';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([AppEffects, HomeEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/store/reducers/index.ts
@@ -0,0 +1,22 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+  MetaReducer
+} from '@ngrx/store';
+import { environment } from '../../../environments/environment';
+import * as fromHome from './home.reducer';
+
+export interface State {
+
+  home: fromHome.State;
+}
+
+export const reducers: ActionReducerMap<State> = {
+
+  home: fromHome.reducer,
+};
+
+
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
